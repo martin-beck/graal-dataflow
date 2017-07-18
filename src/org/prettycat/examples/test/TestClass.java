e7eecd8ce49b6ed5730a2298d4975f8a774587a8
Martin Beck <martin.beck1@tu-dresden.de>
2017-07-18 12:48:21 +0200

    Moved TestClass

---
+++ src/org/prettycat/examples/test/TestClass.java
@@ -0,0 +1,22 @@
+package org.prettycat.examples.test;
+
+public class TestClass {
+	
+	public void call(int i) {
+		i = i + 1;
+		if (i > 0) { 
+			method1(i);
+		} else {
+			method2(i);
+		}
+	}
+
+	public void method1(int i) {
+		method2(-i);
+	}
+	
+	public void method2(int i) {
+		i -= 1;
+		System.out.println(i);
+	}
+}
